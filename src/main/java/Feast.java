39a36ae82b2677f9fdbde0235136a4a1460922db
David Taylor <dave@Davids-MBP.hsd1.ca.comcast.net>
2016-11-09 22:26:39 -0800

    changed the calls to work with new api. I'll need to fix some of hte joournals

--- src/main/java/Feast.java
+++ src/main/java/Feast.java
@@ -9,69 +9,67 @@ import java.io.IOException;
 import org.jsoup.parser.Tag;
 import java.util.ArrayList;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.commons.io.FileUtils;
 import java.lang.StringBuffer;
+import org.apache.commons.lang3.StringEscapeUtils;
 
 class Feast{
 
 	public static void main(String [] args) throws IOException{
 
-		journal1832();
+		//journal1832();
 		bookOfMormon1830();
 		
 	}
 
 	public static void journal1832() throws IOException{
-		// 2 - 106
+
 		StringBuffer string;
 		JsonParser parser = new JsonParser();
-		for(int page = 2; page <= 94; page++){
+
 			string = new StringBuffer();
-			// create the proper api request
-			String url = "http://josephsmithpapers.org/old/api/media-viewer/page?uri=journal-1832-1834&page=" + page + "&pageonly=true&highlight=";
+			//String url = "http://www.josephsmithpapers.org/transcript/journal-1832-1834?nocache";
+			String url = "http://www.josephsmithpapers.org/transcript/journal-1835-1836?nocache";			
+		
 			String json = Jsoup.connect(url).ignoreContentType(true).execute().body();
-			// get the html
-			JsonObject o = parser.parse(json).getAsJsonObject();
-			JsonElement html = o.getAsJsonObject("page").getAsJsonObject("paperSummaryModel").getAsJsonObject("transcriptionModel").get("clear");
-			
+	
 			//parse html for the prophets writings
-			Document doc = Jsoup.parseBodyFragment(html.getAsString()); 
+			Document doc = Jsoup.parseBodyFragment(json); 
 			Elements ele = doc.select("div.indent.wasptag");
+
+			System.out.println(ele.size());
 			for(Element e : ele){
-				string.append(e.text() + " ");
+				string.append(e.text() + "\n");
 			}
 
 			String entry = string.toString();
-			FileUtils.writeStringToFile(new File("build/resources/main/journal_1832_1834/" + page + ".txt"), entry);
-		}
+	
+			FileUtils.writeStringToFile(new File("build/resources/main/journal_1835_1836.txt"), entry);
+
 	}
 
 	public static void bookOfMormon1830() throws IOException{
 		StringBuffer string;
 		JsonParser parser = new JsonParser();
-		// 11 - 594
-		for(int page = 11; page <= 594; page++){
+
+		for(int x = 11; x <= 594; x++){
+			System.out.println(x);
 			string = new StringBuffer();
 			// create the proper api request
-			String url = "http://josephsmithpapers.org/old/api/media-viewer/page?uri=book-of-mormon-1830&page=" + page + "&pageonly=true&highlight=";
+			String url = "http://www.josephsmithpapers.org/paper-summary/book-of-mormon-1830/" + x;
 			String json = Jsoup.connect(url).ignoreContentType(true).execute().body();
-			// get the html
-			JsonObject o = parser.parse(json).getAsJsonObject();
-			JsonElement html = o.getAsJsonObject("page").getAsJsonObject("paperSummaryModel").getAsJsonObject("transcriptionModel").get("clear");
-			
-			//parse html for the book of mormon
-			Document doc = Jsoup.parseBodyFragment(html.getAsString()); 
-			Elements ele = doc.select("div.indent.wasptag");
+
+			Document doc = Jsoup.parseBodyFragment(json); 
+			Elements ele = doc.body().select("div.transcript__content--clear-text > div.indent.wasptag");
 			for(Element e : ele){
 				string.append(e.text() + " ");
 			}
-			//get rid of page numbers
+
 			String entry = string.toString().replaceAll("\\[p.*\\]","");
-			FileUtils.writeStringToFile(new File("build/resources/main/book_of_mormon_1830/" + page + ".txt"), entry);
+			FileUtils.writeStringToFile(new File("build/resources/main/book_of_mormon_1830.txt"), entry, "UTF-8", true);
 		}
 	}
 	
